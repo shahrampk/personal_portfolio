4086cbefed3fb598d96ec0b0372bfe7b0aa5a1e4
Muhammad Shahram <mshahram574@gmail.com>
2025-10-25 21:43:12 +0500

    Portfolio completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -32,5 +32,17 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        ".scrollbar-hide": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+        ".scrollbar-hide::-webkit-scrollbar": {
+          display: "none",
+        },
+      });
+    },
+  ],
 };
