afa26ca6188a382f227d786d3c537852afd86c0b
Muhammad Shahram <mshahram574@gmail.com>
2025-10-27 23:06:14 +0500

    animation added

--- tailwind.config.js
+++ tailwind.config.js
@@ -30,11 +30,103 @@ module.exports = {
         circle: "0 0 196px 60px #DA4DF1",
         custom: "0 5px 16px -2px rgba(0,0,0,0.2)",
       },
+      keyframes: {
+        spin: {
+          "0%": { transform: "rotate(0deg)" },
+          "100%": { transform: "rotate(360deg)" },
+        },
+        appear: {
+          "0%": {
+            opacity: "0",
+            "clip-path": "inset(100% 0 0 0)",
+          },
+          "100%": {
+            opacity: "1",
+            "clip-path": "inset(0 0 0 0)",
+          },
+        },
+        fadeIn: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(-200px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+          },
+        },
+        fadeOut: {
+          "0%": {
+            opacity: "0",
+            transform: "translateX(200px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateX(0px)",
+          },
+        },
+        flowerOpen: {
+          "0%": {
+            transform: "scale(0.2) rotate(0deg)", // Start small and unrotated
+            opacity: "0",
+          },
+          "100%": {
+            transform: "scale(1) rotate(0deg)", // Settle at full size and final position
+            opacity: "1",
+          },
+        },
+        "slide-left": {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        "slide-left-fast": {
+          "0%": { transform: "translateX(-100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        "slide-top": {
+          "0%": { transform: "translateY(-100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "slide-right": {
+          "0%": { transform: "translateX(100%)", opacity: "0" },
+          "100%": { transform: "translateX(0)", opacity: "1" },
+        },
+        "slide-bottom": {
+          "0%": { transform: "translateY(100%)", opacity: "0" },
+          "100%": { transform: "translateY(0)", opacity: "1" },
+        },
+        "circle-Open": {
+          "0%": {
+            opacity: "0",
+            "clip-path": "circle(0% at 50% 50%)",
+          },
+          "100%": {
+            opacity: "1",
+            "clip-path": "circle(100% at 50% 50%)",
+          },
+        },
+      },
+      animation: {
+        appear: "appear 4s forwards",
+        fadeIn: "fadeIn linear 2s",
+        fadeOut: "fadeOut linear 2s",
+        flowerOpen: "flowerOpen 2s forwards",
+        "slide-left": "slide-left 2s forwards", // Adjust duration as needed
+        "slide-right": "slide-right 2s forwards", // Adjust duration as needed
+        "slide-left-fast": "slide-left-fast forwards",
+        "slide-bottom": "slide-bottom 2s forwards",
+        "circle-open": "circle-Open 2s forwards",
+        "slide-top": "slide-top 2s forwards",
+        "spin-slow": "spin 7s linear infinite", // Adjust the duration (5 seconds)
+        // "animation-timeline": "view()",
+        // "animation-range": " entry 0% cover 40%",
+      },
     },
   },
   plugins: [
     function ({ addUtilities }) {
-      addUtilities({
+      const newUtilities = {
+        /* Hide scrollbar */
         ".scrollbar-hide": {
           "-ms-overflow-style": "none",
           "scrollbar-width": "none",
@@ -42,7 +134,16 @@ module.exports = {
         ".scrollbar-hide::-webkit-scrollbar": {
           display: "none",
         },
-      });
+
+        /* Scroll animation */
+        ".scroll-animate": {
+          animationTimeline: "view()",
+          animationRange: "entry 0% cover 30%",
+        },
+      };
+
+      // Add utilities with responsive and hover variants
+      addUtilities(newUtilities, ["responsive", "hover"]);
     },
   ],
 };
